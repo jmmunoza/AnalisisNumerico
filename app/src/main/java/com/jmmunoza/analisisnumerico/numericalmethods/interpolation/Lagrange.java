6dc44444e142b68c89edf214569bd647f32fc585
jmmunoza <jmmunoza@eafit.edu.co>
2022-05-28 18:16:13 -0500

    Lagrange

--- app/src/main/java/com/jmmunoza/analisisnumerico/numericalmethods/interpolation/Lagrange.java
+++ app/src/main/java/com/jmmunoza/analisisnumerico/numericalmethods/interpolation/Lagrange.java
@@ -4,9 +4,13 @@ public class Lagrange {
     public static double[] lagrange(double[] x, double[] y, double[] x_values){
         if(x.length == y.length){
             double[] results = new double[x_values.length];
-            for(int i = 0; i < x_values.length; i++){
+            for(int i = 0; i < x_values.length; i++) {
+                // Evaluando cada x_value
+
+                // Polinomio de lagrange
                 double L = 0;
                 for(int j = 0; j < x.length; j++){
+                    // Bases polinomicas de Lagrange
                     double l = 1;
                     for(int k = 0; k < x.length; k++){
                         if(k != j)
